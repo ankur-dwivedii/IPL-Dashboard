9c1e554d6d107483e7e2c138077170b5ed925479
Darklord3322 <ankurdwivedixxx@gmail.com>
2023-08-10 21:58:58 +0530

    Added react router

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+//Importing router to route teams/teamName to TeamPage component
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {  
   return (
     <div className="App">
-      <TeamPage />
+      <Router>
+        <Routes>
+          //Routing Req to TeamPage using teamName present in the request URL
+          <Route path="/teams/:teamName" element={<TeamPage/>} />
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
-// new text
+
 export default App;
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,15 +1,16 @@
 import { React, useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
 
-  const [team, setTeam] = useState({matches : []});
-
-  useEffect(
+  const [team, setTeam] = useState({matches : []});//useState will store team data into team by grabbing it from setTeam fxn below
+  const { teamName } = useParams();//This will grab the teamName from req URL
+  useEffect(//useEffect runs the given code if there is any change in the react
     () => {
       const fetchMatches = async () => {
-          const response = await fetch('http://localhost:8080/team/Rajasthan Royals');
+          const response = await fetch(`http://localhost:8080/team/${teamName}`);
           const data = await response.json();
           //console.log(data);
           setTeam(data);
@@ -17,15 +18,17 @@ export const TeamPage = () => {
       fetchMatches();
 
 
-    }, []
+    }, []//this empty array will stop effect to run infinite times and thus it only run once
     
   );
-
+if (!team || !team.teamName){
+    return <h1>Team Not Found</h1>
+}//if teamName is incorrect we will handle error this way
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}/>
-      {team.matches.slice(1).map(match=> <MatchSmallCard match={match} />)}
+      <MatchDetailCard match={team.matches[0]}/>//Showing 1st match here
+      {team.matches.slice(1).map(match=> <MatchSmallCard match={match} />)}//slice will ignore 1st match and map will show other 3
       
     </div>
   );
