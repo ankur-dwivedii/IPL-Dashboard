ec4cf8733741b32612f5c8df2af0e1472e71879f
Darklord3322 <ankurdwivedixxx@gmail.com>
2023-08-14 22:47:52 +0530

    Added match page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
+import "./App.css";
 //Importing router to route teams/teamName to TeamPage component
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { TeamPage } from './pages/TeamPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { TeamPage } from "./pages/TeamPage";
+import { MatchPage } from "./pages/MatchPage";
 
-function App() {  
+function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           //Routing Req to TeamPage using teamName present in the request URL
-          <Route path="/teams/:teamName" element={<TeamPage/>} />
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
         </Routes>
       </Router>
-      
     </div>
   );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,35 +1,36 @@
 import { React, useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
-
-  const [team, setTeam] = useState({matches : []});//useState will store team data into team by grabbing it from setTeam fxn below
-  const { teamName } = useParams();//This will grab the teamName from req URL
-  useEffect(//useEffect runs the given code if there is any change in the react
+  const [team, setTeam] = useState({ matches: [] }); //useState will store team data into team by grabbing it from setTeam fxn below
+  const { teamName } = useParams(); //This will grab the teamName from req URL
+  useEffect(
+    //useEffect runs the given code if there is any change in the react
     () => {
       const fetchMatches = async () => {
-          const response = await fetch(`http://localhost:8080/team/${teamName}`);
-          const data = await response.json();
-          //console.log(data);
-          setTeam(data);
+        const response = await fetch(`http://localhost:8080/team/${teamName}`);
+        const data = await response.json();
+        //console.log(data);
+        setTeam(data);
       };
       fetchMatches();
-
-
-    }, []//this empty array will stop effect to run infinite times and thus it only run once
-    
+    }, [teamName] //this empty array will stop effect to run infinite times and thus it only run once, adding 
+    // teamName so it changes when teamName changes(when we click on teams name hyperlink)
   );
-if (!team || !team.teamName){
-    return <h1>Team Not Found</h1>
-}//if teamName is incorrect we will handle error this way
+  if (!team || !team.teamName) {
+    return <h1>Team Not Found</h1>;
+  } //if teamName is incorrect we will handle error this way
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}/>//Showing 1st match here
-      {team.matches.slice(1).map(match=> <MatchSmallCard match={match} />)}//slice will ignore 1st match and map will show other 3
-      
+      {/* Showing 1st match here */}
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      {/* slice will ignore 1st match and map will show other 3 */}
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard teamName={team.teamName} match={match} />
+      ))}
     </div>
   );
 };
